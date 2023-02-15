13ced5bc187fc191064c1d648bde25b9e0e6cace
Larsa-mrp <bradleyspiclin@gmail.com>
2023-02-15 22:25:43 +1100

    added form submission & download link for CV

--- app.js
+++ app.js
@@ -11,3 +11,16 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+// on click event to submit contact form ensuring all fields are filled in
+document.getElementById("submit-form").addEventListener("click", function(event) {
+    event.preventDefault(); // prevent the default behavior of the anchor link
+    if (document.getElementById("name").value !== '' &&
+      document.getElementById("email").value !== '' &&
+      document.getElementById("message").value !== '' &&
+      document.getElementById("subject").value !== '') {
+    document.getElementById("my-form").submit(); // submit the form
+  } else {
+    alert("Please fill in all required fields."); // show an error message
+  }
+})();
